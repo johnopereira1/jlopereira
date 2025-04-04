6841c0ca790416ebf5afe22acdd42ed40a5d9d62
johnopereira1 <johnlenondeoliveirapereira@gmail.com>
2025-04-04 13:34:37 -0300

    Update strunfomestre.c

--- strunfomestre.c
+++ strunfomestre.c
@@ -11,9 +11,9 @@ int main() {
     int pontoturistico1, pontoturistico2;
 
     // Cálculos
-    float densidade1, densidade2;
-    float pibpercapita1, pibpercapita2;
-    float superpoder1, superpoder2;
+    float densidade1 = 0, densidade2 = 0;
+    float pibpercapita1 = 0, pibpercapita2 = 0;
+    float superpoder1 = 0, superpoder2 = 0;
 
     // Entrada - Carta 1
     printf("Cadastro de Cartas do Super Trunfo - Carta 1:\n");
@@ -21,6 +21,7 @@ int main() {
     scanf(" %c", &estado1);
     printf("Digite o código da carta (ex: A01, B03):\n");
     scanf(" %9s", codigo1);
+    getchar(); // Limpar buffer
     printf("Digite o nome da cidade:\n");
     scanf(" %49[^\n]", nomecidade1);
     printf("Digite a população:\n");
@@ -38,6 +39,7 @@ int main() {
     scanf(" %c", &estado2);
     printf("Digite o código da carta (ex: A01, B03):\n");
     scanf(" %9s", codigo2);
+    getchar(); // Limpar buffer
     printf("Digite o nome da cidade:\n");
     scanf(" %49[^\n]", nomecidade2);
     printf("Digite a população:\n");
@@ -49,6 +51,12 @@ int main() {
     printf("Digite o número de pontos turísticos:\n");
     scanf("%d", &pontoturistico2);
 
+    // Verificação de áreas válidas
+    if (area1 == 0 || area2 == 0) {
+        printf("Erro: A área de uma das cidades é zero. Não é possível calcular densidade.\n");
+        return 1;
+    }
+
     // Cálculos
     densidade1 = populacao1 / area1;
     densidade2 = populacao2 / area2;
@@ -56,8 +64,8 @@ int main() {
     pibpercapita1 = (pib1 * 1000000000) / populacao1;
     pibpercapita2 = (pib2 * 1000000000) / populacao2;
 
-    superpoder1 = (float)populacao1 + area1 + pib1 + pontoturistico1 + pibpercapita1 + (1 / densidade1);
-    superpoder2 = (float)populacao2 + area2 + pib2 + pontoturistico2 + pibpercapita2 + (1 / densidade2);
+    superpoder1 = populacao1 + area1 + pib1 + pontoturistico1 + pibpercapita1 + (1 / densidade1);
+    superpoder2 = populacao2 + area2 + pib2 + pontoturistico2 + pibpercapita2 + (1 / densidade2);
 
     // Exibição das cartas
     printf("\nCarta 1:\n");
@@ -86,13 +94,13 @@ int main() {
 
     // Comparações
     printf("\nComparação de Cartas:\n");
-    printf("População: Carta 1 venceu (%d)\n", populacao1 > populacao2);
-    printf("Área: Carta 1 venceu (%d)\n", area1 > area2);
-    printf("PIB: Carta 1 venceu (%d)\n", pib1 > pib2);
-    printf("Pontos Turísticos: Carta 1 venceu (%d)\n", pontoturistico1 > pontoturistico2);
-    printf("Densidade Populacional: Carta 1 venceu (%d)\n", densidade1 < densidade2); // MENOR vence
-    printf("PIB per Capita: Carta 1 venceu (%d)\n", pibpercapita1 > pibpercapita2);
-    printf("Super Poder: Carta 1 vAenceu (%d)\n", superpoder1 > superpoder2);
+    printf("População: %s\n", (populacao1 > populacao2) ? "Carta 1 venceu" : "Carta 2 venceu");
+    printf("Área: %s\n", (area1 > area2) ? "Carta 1 venceu" : "Carta 2 venceu");
+    printf("PIB: %s\n", (pib1 > pib2) ? "Carta 1 venceu" : "Carta 2 venceu");
+    printf("Pontos Turísticos: %s\n", (pontoturistico1 > pontoturistico2) ? "Carta 1 venceu" : "Carta 2 venceu");
+    printf("Densidade Populacional: %s\n", (densidade1 < densidade2) ? "Carta 1 venceu (menor densidade)" : "Carta 2 venceu (menor densidade)");
+    printf("PIB per Capita: %s\n", (pibpercapita1 > pibpercapita2) ? "Carta 1 venceu" : "Carta 2 venceu");
+    printf("Super Poder: %s\n", (superpoder1 > superpoder2) ? "Carta 1 venceu" : "Carta 2 venceu");
 
     return 0;
 }
